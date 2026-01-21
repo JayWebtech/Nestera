f74a6e2fe27d3dc47d393c9b2db4e24b7ba7dc52
pope-h <hamatthan@gmail.com>
2026-01-22 00:18:32 +0100

    ran test for user instantiation

--- contracts/src/test.rs
+++ contracts/src/test.rs
@@ -1,7 +1,15 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{vec, Env, String};
+use soroban_sdk::{symbol_short, testutils::Address as _, Address, Env};
 
 #[test]
-fn test() {}
+fn test_user_instantiation() {
+    let user = User {
+        total_balance: 1_000_000,
+        savings_count: 3,
+    };
+    
+    assert_eq!(user.total_balance, 1_000_000);
+    assert_eq!(user.savings_count, 3);
+}
